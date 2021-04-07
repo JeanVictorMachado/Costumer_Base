ecdb7df6d2dfb7513c91077be08d3444540609a9
Jean Machado <jeanvictor.arq@gmail.com>
2021-04-07 19:42:32 -0300

    :memo: created structure app

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+
+import light from './styles/Theme/light';
+
+import Routes from './routes';
+
+const App: React.FC = () => {
+  return (
+    <ThemeProvider theme={light}>
+      <GlobalStyles />
+      <Routes />
+    </ThemeProvider>
+  );
+};
+
+export default App;
